999d0a02445aa7427087dbad81cb4b7f447cea71
alef.soares <alef.soares@squadra.com.br>
2024-09-30 10:27:51 -0300

    [TESTE]: Atualizando o teste

--- tests/WebApi.Tests/Users/Delete/DeleteUserTest.cs
+++ tests/WebApi.Tests/Users/Delete/DeleteUserTest.cs
@@ -5,17 +5,17 @@ namespace WebApi.Tests.Users.Delete;
 public class DeleteUserTest : CashFlowClassFixture
 {
     private const string REQUEST_URI = "/api/users";
-    private readonly string _userTeamMemberToken;
+    private readonly string _userAdminToken;
 
     public DeleteUserTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
     {
-        _userTeamMemberToken = webApplicationFactory.UserTeamMember.GetToken();
+        _userAdminToken = webApplicationFactory.UserAdmin.GetToken();
     }
 
     [Fact]
     public async Task Success()
     {
-        var response = await DoDelete(REQUEST_URI, _userTeamMemberToken);
+        var response = await DoDelete(REQUEST_URI, _userAdminToken);
 
         response.Should().BeSuccessful();
         response.StatusCode.Should().Be(HttpStatusCode.NoContent);
